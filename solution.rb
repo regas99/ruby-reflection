e404c5db6c82beaae875e576f98f9f6738df166b
Tom Sager <tom.f.sager@gmail.com>
2015-08-21 13:37:03 -0700

    removed puts

--- solution.rb
+++ solution.rb
@@ -24,30 +24,23 @@ module Spy
   def self.set_methud
     klazz = self.set_klass
     klazz::class_eval do
-      puts "methud defined? #{method_defined?(Spy.methud)}"
       if method_defined?(Spy.methud)
         # save an existing method
-        puts "...aliasing..."
         alias_method :real_method, Spy.methud
       else
         # or create a noop method
-        puts "...new noop method..."
         define_method :real_method do; end;
       end
-      puts "defining #{Spy.methud} over #{klazz}"
       old = klazz.methods
       define_method Spy.methud do
-        puts "executing..."
         Spy.increment
         real_method
       end
       new = klazz.methods
-      puts "new methods: #{new-old}"
     end
   end
 
   def self.initialize
-    puts "initializing Spy for #{ENV["COUNT_CALLS_TO"]}"
     @count = 0
     @klass, @methud = ENV['COUNT_CALLS_TO'].split('#')
     set_methud
@@ -60,9 +53,6 @@ module Spy
   # instantiate singleton
   if ENV["COUNT_CALLS_TO"]
     @@instance = Spy.initialize
-    puts "singleton is #{@@instance.inspect}"
-  else
-    puts "Spy module called with no ENV['COUNT_CALLS_TO'] set"
   end
 
 
