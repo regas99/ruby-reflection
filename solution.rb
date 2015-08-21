92e4b9260d86b05b0b2fe21a311736780b6148b2
Tom Sager <tom.f.sager@gmail.com>
2015-08-21 13:14:24 -0700

    evolving - set_klass and set_methud defined but not debugged

--- solution.rb
+++ solution.rb
@@ -1,49 +1,71 @@
 module Spy
-  attr_reader :count, :class, :method
-
-  def self.initialize
-    puts "initializing Spy for #{ENV["COUNT_CALLS_TO"]}"
-    @count = 0
-    @class, @method = ENV['COUNT_CALLS_TO'].split('#')
-    puts "#{@class}##{@method} => #{@count}"
+  def self.count
+    @count
   end
-
+  def self.klass
+    @klass
+  end
+  def self.methud
+    @methud
+  end
   def self.increment
     @count += 1
   end
 
-  # instantiate singleton
-  if ENV["COUNT_CALLS_TO"]
-    @@instance = self.initialize
-  else
-    puts "Spy module called with no ENV['COUNT_CALLS_TO'] set"
-  end
-
-  def get_class
-    # set or create the class
-    if Object.const_defined?(@class)
-      Object.const_get(@class)
+  def self.set_klass
+    # set or create the klass
+    if Object.const_defined?(@klass)
+      Object.const_get(@klass)
     else
-      Object.const_set(@class, Class.new)
+      Object.const_set(@klass, Class.new)
     end
   end
 
-  def set_method
-    klazz = self.get_class
+  def self.set_methud
+    klazz = self.set_klass
     klazz::class_eval do
-      if method_defined?(@method)
+      if method_defined?(Spy.methud)
         # save an existing method
-        alias_method :real_method, @method
+        alias_method :real_method, Spy.methud
       else
         # or create a noop method
         define_method :real_method do; end;
       end
     end
-    define_method @method do
+    puts "methud is #{Spy.methud}"
+    define_method Spy.methud do
       increment
       real_method
     end
   end
 
+  def self.initialize
+    puts "initializing Spy for #{ENV["COUNT_CALLS_TO"]}"
+    @count = 0
+    @klass, @methud = ENV['COUNT_CALLS_TO'].split('#')
+    set_methud
+    self
+  end
+
+
+
+
+  # instantiate singleton
+  if ENV["COUNT_CALLS_TO"]
+    @@instance = Spy.initialize
+    puts "singleton is #{@@instance.inspect}"
+  else
+    puts "Spy module called with no ENV['COUNT_CALLS_TO'] set"
+  end
+
+
+
 end
+
+ObjectSpace.define_finalizer(
+  ENV,
+  proc{ |id| puts "#{Spy.klass}# #{Spy.methud} called #{Spy.count} times" }
+  )
+
+
 
