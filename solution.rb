2fc95767eba116323168e3e2cd5abc5960388830
Tom Sager <tom.f.sager@gmail.com>
2015-08-21 11:34:31 -0700

    first pass at overriding class and method

--- solution.rb
+++ solution.rb
@@ -8,10 +8,41 @@ module Spy
     puts "#{@class}##{@method} => #{@count}"
   end
 
+  def self.increment
+    @count += 1
+  end
+
+  # instantiate singleton
   if ENV["COUNT_CALLS_TO"]
     @@instance = self.initialize
   else
     puts "Spy module called with no ENV['COUNT_CALLS_TO'] set"
+  end
+
+  def get_class
+    # set or create the class
+    if Object.const_defined?(@class)
+      Object.const_get(@class)
+    else
+      Object.const_set(@class, Class.new)
+    end
+  end
+
+  def set_method
+    klazz = self.get_class
+    klazz::class_eval do
+      if method_defined?(@method)
+        # save an existing method
+        alias_method :real_method, @method
+      else
+        # or create a noop method
+        define_method :real_method do; end;
+      end
+    end
+    define_method @method do
+      increment
+      real_method
+    end
   end
 
 end
